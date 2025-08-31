6daa590d8c353387a8c1838cfb1f783599647ca8
Oscar <albertlund121@gmail.com>
2025-08-31 22:42:52 +0200

    Improves layout responsiveness and adjusts text width
    Enhances the responsiveness of the header layout by introducing
    flex direction adjustments for better handling of small screens.
    
    Refines text container width for a more consistent appearance
    and ensures the button section does not shrink unexpectedly.
    
    These changes improve the visual structure and usability across
    different viewport sizes.

--- src/app/components/layout/FrontpageHeaderSection.jsx
+++ src/app/components/layout/FrontpageHeaderSection.jsx
@@ -15,23 +15,23 @@ const HeaderSection = ({
   return (
     <div className={` ${borderClass} pb-16`}>
       <div className="content-max">
-        <div className="flex justify-between items-end">
+        <div className="flex justify-between md:flex-row flex-col items-end">
           <div className=" space-y-8">
             <div className="w-full ">
               <h2>{title}</h2>
             </div>
             <div className="flex flex-col md:flex-row md:justify-between md:items-end mt-4 md:mt-10 gap-4">
-              <p className=" w-full md:w-xl">{text}</p>
+              <p className=" w-full md:w-lg">{text}</p>
 
+            </div>
+          </div>
               {variant !== false && (
-                <div className=" items-end mt-6 sm:mt-2">
+                <div className=" items-end flex-shrink-0 mt-6 sm:mt-2">
                   <Link href={buttonHref}>
                     <Button variant={variant}>{buttonText}</Button>
                   </Link>
                 </div>
               )}
-            </div>
-          </div>
         </div>
       </div>
     </div>
